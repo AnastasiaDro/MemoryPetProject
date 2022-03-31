bea55d8517b0156e0c1a7e0c0b17af2deae2a0ae
Anastasia Drogunova <adrogunova@gmail.com>
2022-04-01 00:34:58 +0300

    TenWords: - подключение liveData, layout фрагмента

--- app/src/main/java/com/nestdev/memorypetproject/TenWordsFragment.kt
+++ app/src/main/java/com/nestdev/memorypetproject/TenWordsFragment.kt
@@ -1,18 +1,36 @@
 package com.nestdev.memorypetproject
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TableLayout
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 
 class TenWordsFragment : Fragment(R.layout.fragment_ten_words) {
-    private lateinit var viewModel: TenWordsViewModel
+    private val viewModel = TenWordsViewModel by viewModels()
+    private var counter = 0
+    private var currentWordIndex = -1
     private lateinit var tableLayout: TableLayout
     private lateinit var wordsArray: List<String>
-    private lateinit var btnsArray: List<String>
+    private lateinit var btnsArray: List<Button>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(TenWordsViewModel::class.java)
+        val counterObserver = Observer<Int> { newCounter ->
+            counter = newCounter
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initButtons()
+    }
+
+    fun initButtons() {
+        btnsArray = listOf(R.id.)
+    }
+
+    companion object {
+        fun create() = TenWordsFragment()
     }
 }
--- app/src/main/java/com/nestdev/memorypetproject/TenWordsViewModel.kt
+++ app/src/main/java/com/nestdev/memorypetproject/TenWordsViewModel.kt
@@ -1,7 +1,21 @@
 package com.nestdev.memorypetproject
 
+import android.content.Context
+import android.view.View
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class TenWordsViewModel : ViewModel() {
+    private val mutableCounterData by lazy {
+        MutableLiveData<Int>()
+    }
+    val counterData : LiveData<Int> = mutableCounterData
 
+
+    var counter = 0
+
+    fun onButtonPressed() {
+        mutableCounterData.setValue(++counter)
+    }
 }
