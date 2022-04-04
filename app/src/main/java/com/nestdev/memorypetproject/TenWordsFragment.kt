90e4f833ff8d069877c2d6ea2ef7bf29988e3a52
Anastasia Drogunova <adrogunova@gmail.com>
2022-04-04 12:03:01 +0300

    TenWords: - проинициализировала массивы textView

--- app/src/main/java/com/nestdev/memorypetproject/TenWordsFragment.kt
+++ app/src/main/java/com/nestdev/memorypetproject/TenWordsFragment.kt
@@ -27,7 +27,7 @@ class TenWordsFragment : Fragment() {
      * Слова
      */
     private lateinit var wordsArray: List<String>
-  //  private lateinit var wordsTextViewArray:
+    private lateinit var wordsTextViewArray: Array<TextView>
 
     /**
      * textView для проб
@@ -40,7 +40,7 @@ class TenWordsFragment : Fragment() {
     /**
      * Отсроченное воспроизведение
      */
-    private lateinit var deferredArray: List<TextView>
+    private lateinit var deferredTrialArray: Array<TextView>
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -57,9 +57,20 @@ class TenWordsFragment : Fragment() {
         return binding.root
     }
 
-    private fun initTextViews(view: View) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initTextViewArrays(view)
+    }
+
+    private fun initTextViewArrays(view: View) {
         with(binding) {
-            tiral0Array[0] =
+            wordsTextViewArray = arrayOf(word0, word1, word2, word3, word4, word5, word6, word7, word8, word9)
+            tiral0Array = arrayOf(trial00, trial01, trial02, trial03, trial04, trial05, trial06, trial07, trial08, trial09)
+            tiral1Array = arrayOf(trial10, trial11, trial12, trial13, trial14, trial15, trial16, trial17, trial18, trial19)
+            tiral2Array = arrayOf(trial20, trial21, trial22, trial23, trial24, trial25, trial26, trial27, trial28, trial29)
+            tiral3Array = arrayOf(trial30, trial31, trial32, trial33, trial34, trial35, trial36, trial37, trial38, trial39)
+            deferredTrialArray = arrayOf(deferred0, deferred1, deferred2, deferred3, deferred4, deferred5, deferred6, deferred7, deferred8, deferred9)
+
         }
     }
 
