3a189a765be364dab75a86f24deb0d524e0998d0
Anastasia Drogunova <adrogunova@gmail.com>
2022-04-04 12:09:34 +0300

    TenWords: - подключила фрагмент к Активити TODO: ContentProvider: - создать базу данных со словами - заполнить сетами слов - выводить слова из базы данных на экран (подумать про splash-фрагмент на время загрузки слов)

--- app/src/main/java/com/nestdev/memorypetproject/MainActivity.kt
+++ app/src/main/java/com/nestdev/memorypetproject/MainActivity.kt
@@ -7,10 +7,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        launchTenWordsFragment()
     }
 
-    fun loadWords()
-    {
 
+    private fun launchTenWordsFragment() {
+        supportFragmentManager.beginTransaction().apply {
+            setReorderingAllowed(true) //TODO  погуглить про свойство
+            replace(R.id.main_activity_container, TenWordsFragment.create())
+            commitAllowingStateLoss()
+        }
     }
 }
